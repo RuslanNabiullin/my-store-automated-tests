e44c10aa2820897eece6103ac4d988c319295d75
rnabiullin <rnabiullin@intermedia.com>
2022-09-16 13:06:59 +0400

    Extraxt get properties to another method

--- src/test/java/my/store/qa/world/Environment.java
+++ src/test/java/my/store/qa/world/Environment.java
@@ -9,11 +9,7 @@ public class Environment {
 
         try {
 
-            InputStream inputStream = Environment.class.getResourceAsStream("/environments/DriverConfiguration.properties");
-
-            Properties properties = new Properties();
-            properties.load(inputStream);
-
+            Properties properties = getProperties("/environments/DriverConfiguration.properties");
             return new DriverConfiguration(properties);
 
         } catch (Exception e) {
@@ -25,16 +21,29 @@ public class Environment {
 
         try {
 
-            InputStream inputStream = Environment.class.getResourceAsStream("/environments/MyStoreConfiguration.properties");
-
-            Properties properties = new Properties();
-            properties.load(inputStream);
-
+            Properties properties = getProperties("/environments/MyStoreConfiguration.properties");
             return new MyStoreConfiguration(properties);
 
         } catch (Exception e) {
             throw new RuntimeException("Unable to get driver configuration. " + e);
         }
+    }
+
+    private static Properties getProperties(String path) {
+
+        try {
+
+            InputStream inputStream = Environment.class.getResourceAsStream(path);
+
+            Properties properties = new Properties();
+            properties.load(inputStream);
+
+            return properties;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to get property configuration. " + e);
+        }
+
     }
 
 }
